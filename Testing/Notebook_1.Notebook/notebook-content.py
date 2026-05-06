998a3682d7309a4bbf95ecec6aab5f946a26f32c
AmanDataEngineer <amanchavan133@gmail.com>
2026-05-06 19:41:11 +0000

    Committing 25 items from workspace fa681d93-47f5-429d-aad1-1f8bbbad0a6a

---
+++ Testing/Notebook_1.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+
+import pandas as pd
+
+# Sample data
+data = {
+    "Name": ["Aman", "Rahul", "Neha"],
+    "Score": [85, 90, 95]
+}
+
+df = pd.DataFrame(data)
+
+# Show dataframe
+df
+
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
